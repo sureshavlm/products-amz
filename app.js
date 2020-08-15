8fca9615aa744b0658e031457070095338b78e8a
suresh <suresh.avlm@gmail.com>
2020-08-15 08:06:21 +0530

    enabling CORS

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true); 
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/v1/api/products', prodRoutes);
 
 app.use('/', (req, res)=> {
