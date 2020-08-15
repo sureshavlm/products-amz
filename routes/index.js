ace1a89a1fb7013c1b0d4c72c94eb970d2fcbc0e
suresh <suresh.avlm@gmail.com>
2020-08-15 08:11:19 +0530

    adde a new products

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/all', (req, res) => {
-	res.json(['Macbook Pro', 'Lenovo']);
+	res.json(['Macbook Pro', 'Lenovo', 'Sony']);
 });
 
 router.get('/add', (req, res) => {
